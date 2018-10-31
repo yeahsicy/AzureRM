43ec09665962562d7c2fc4c02612a242fe65abe0
Jerry Ye <34499027+yeahsicy@users.noreply.github.com>
2018-10-31 00:32:45 -0700

    Initial commit

---
+++ Azure_instance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+
+//https://github.com/Azure/azure-libraries-for-net
+
+namespace AzureRM
+{
+    class Azure_instance
+    {
+        IAzure azure;
+
+        public Azure_instance(string subscriptionId, string clientId, string clientSecret, string tenantId, AzureEnvironment environment)
+        {
+            var c = SdkContext.AzureCredentialsFactory.FromServicePrincipal(clientId, clientSecret, tenantId, environment);
+            azure = Azure.Authenticate(c).WithSubscription(subscriptionId);
+        }
+
+        public IEnumerable<string> GetResourceGroupNames()
+        {
+            var ResourceGroups = azure.ResourceGroups.List();
+            return ResourceGroups.Select(r => r.Name);
+        }
+
+        /// <summary>
+        /// Delete Resource Groups using the pattern
+        /// </summary>
+        /// <param name="pattern">For example, exact search "test"; start with "test*"; end with "*test"; contain "*test*"</param>
+        public void DeleteResourceGroups(string pattern)
+        {
+            var names = GetResourceGroupNames();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace AzureRM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Azure_instance azure = new Azure_instance("", "", "", "", AzureEnvironment.AzureGlobalCloud);
+        }
+    }
+}
